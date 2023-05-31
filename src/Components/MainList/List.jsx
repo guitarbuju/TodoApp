f9b57900c8ad1514b1aa63952b64ab609c3fd0bb
guitarbuju <gerardoamadrid@gmail.com>
2023-05-31 09:48:49 +0200

    delete request added

--- src/Components/MainList/List.jsx
+++ src/Components/MainList/List.jsx
@@ -1,82 +1,95 @@
 // eslint-disable-next-line no-unused-vars
-import {useState,useEffect,React} from 'react'
-import styles from './list.module.css'
-import Head from './Head'
-
+import { useState, useEffect, React } from "react";
+import styles from "./list.module.css";
+import Head from "./Head";
 
 const List = () => {
- 
-const[Lista,setLista]=useState([])
+  const [Lista, setLista] = useState([]);
 
+  ///////////////FETCH LISTA GENERAL///////////////
+  const daList = async () => {
+    const response = await fetch("http://Localhost:3006/");
+    const data = await response.json();
+    const sortedList = data.sort((a, b) => b - a).reverse();
 
-///////////////FETCH LISTA GENERAL///////////////
-  const daList=async()=>{ 
-    
-         const response= await fetch('http://Localhost:3006/')
-         const data= await response.json()
-         const sortedList=data.sort((a, b) =>b -a).reverse();
-        
-         setLista(sortedList)
-      
+    setLista(sortedList);
+  };
+
+  useEffect(() => {
+    daList();
+  }, [Lista]);
+
+  /////PATCH TASKS TO DONE
+  const [_id, setId] = useState("");
+
+  const handleChange = (event) => {
+    setId(event.target.value)
+  };
+
+  useEffect(() => {
+    if (_id) {
+      PatchNew1()
     }
-   
-    useEffect(() => {
+  }, [_id])
+
+  const PatchNew1 = async () => {
+    const requestedOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const response = await fetch(
+      `http://Localhost:3006/done/${_id}`,
+      requestedOptions
+    );
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      // If the delete request is successful, update the list
       daList()
-    }, [Lista])
-
-
-    /////PATCH TASKS TO DONE
-    const [_id, setId] = useState('')
-    
-    const handleChange = (event) => {
-      setId(event.target.value);}
-     
-      useEffect(() => {
-        if (_id) {
-          PatchNew1();
-        }
-      }, [_id]);
-      
-    
-    const PatchNew1= async()=>{
-
-      const requestedOptions={
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        
-      };
-     
-      const response= await fetch(`http://Localhost:3006/done/${_id}`,requestedOptions)
-      const data= await response.json()
-      console.log(data)
+    }
+  };
+  // If the patch request is successful, update the list
   
-      }
-    
-      
+
+ 
+  
+  const handleDelete = async (_id) => {
+    const requestOptions = {
+      method: 'DELETE',
+    };
+
+    const response = await fetch(`http://localhost:3006/delete/${_id}`, requestOptions);
+    if (response.ok) {
+      // If the delete request is successful, update the list
+      daList()
+    }
+  };
 
   return (
     <>
-      <Head/>  
-    <div className={styles.wrapper}>
+      <Head />
+      <div className={styles.wrapper}>
         <ul className={styles.list_container}>
-       
-        {Lista.map((element,index)=><li 
-        key={index}
-        className={!element.done ? styles.element : styles.done}
-      
-        >
-        <input
-        value={element._id} 
-        onChange={handleChange}
-       
-        disabled={element.done ? 'disabled':''} type='radio'/>
-        <p >{element.task}</p></li>)}
-      
+          {Lista.map((element, index) => (
+            <li
+              onDoubleClick={() => handleDelete(element._id)}
+              key={index}
+              className={!element.done ? styles.element : styles.done}
+            >
+              <input
+                value={element._id}
+                onChange={handleChange}
+                disabled={element.done ? "disabled" : ""}
+                type="radio"
+              />
+              <p>{element.task}</p>
+            </li>
+          ))}
         </ul>
-           
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default List
+export default List;
