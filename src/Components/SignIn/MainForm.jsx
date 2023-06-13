e8462def4ece07511a227b93265bf3d8406a44cd
guitarbuju <gerardoamadrid@gmail.com>
2023-06-13 12:07:26 +0200

    fixed log in and register

--- src/Components/SignIn/MainForm.jsx
+++ src/Components/SignIn/MainForm.jsx
@@ -4,100 +4,63 @@ import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { Link } from "react-router-dom";
 import axios from 'axios'
-import { setItemToLocalStorage} from '../../assets/utils.js'
 import { useNavigate } from 'react-router-dom';
-
+import { useForm } from "react-hook-form"
 
 const MainForm = () => {
-
-  const [answer,setanswer]=useState('')
-  const [name, setname] = useState('')
-  const [email, setemail] = useState('')
-  const [password, setpassword] = useState('')
+// eslint-disable-next-line no-unused-vars
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
  const navigate=useNavigate()
  
-const Register=(e)=>{
+ const onSubmit= async(data)=>{
+  console.log(data)
  
-  e.preventDefault();
-    const obj = { name, email, password }
-    
-    console.log(obj)
- 
-   
- 
-    
- 
-    const signIn = async () => {
+  try {
+    const response = await axios.post('http://localhost:3006/register', data);
+    const { token } = response.data;
+    console.log(response.data) 
+    // Save the token to local storage
+    localStorage.setItem('token', token);
 
-      
-      try {
-        const response = await axios.post('http://Localhost:3006/register',
-        obj,{ headers:{"Content-Type": "application/json"} });
-        // eslint-disable-next-line no-unused-vars
-       const data= await response.data
+    navigate('/today')
 
-       const{token,user}=data
-
-        setItemToLocalStorage('token',token)
-        setItemToLocalStorage('user',user)
-
-        navigate('/today')
-
-     
-       console.log(data.token)
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    signIn()
-
-
-    setname('')
-    setemail('')
-    setpassword('')
-   
-    function runFunctionWithDuration(duration) {
-      
-    setanswer('Usuario agregado con exito')
-      setTimeout(function() {
-        setanswer('') 
-      }, duration);}
-    
-    
-    // Example usage:
-    runFunctionWithDuration(5000);
-   
+    console.log('Logged in successfully');
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+ 
+ 
 
 
   return (
     <>
-    <form name='miForm' >
+    <form name='regForm' onSubmit={handleSubmit(onSubmit)} >
       <div className={styles.inputData}>
           <h1>Sign In</h1>
         <div className={styles.dainput}>
             <label htmlFor="name" style={{fontWeight:'bolder'}}>NAME</label>
-            <input value={name} className='form-control'   name='name'
-             id='name' type='text' placeholder= 'input your username'
-              onChange={(e)=>{setname(e.target.value)}}
+            <input  className='form-control'  
+              type='text' placeholder= 'input your username'
+             {...register("name", { required: true })}
              />
         </div>
         
         <div className={styles.dainput}>
             <label htmlFor="email" style={{fontWeight:'bolder'}}>EMAIL</label>
-            <input  value={email} className="form-control" name='email'
-            id='email' type='text' placeholder='input your email'
-              onChange={(e)=>{setemail(e.target.value)}}
+            <input  className="form-control"
+             type='email' placeholder='input your email'
+            {...register("email", { required: true })}
             />
         </div>
         
        <div className={styles.dainput}>
             <label htmlFor="password" style={{fontWeight:'bolder'}}>PASSWORD</label>
-            <input value={password} className="form-control" name='password'
-            id='password' type='text'  placeholder='input your password'
-              onChange={(e)=>{setpassword(e.target.value)}}
+            <input  className="form-control"
+            type='password'  placeholder='input your password'
+            {...register("password", { required: true })}
             />
        </div>
         
@@ -105,16 +68,15 @@ const Register=(e)=>{
 
      <div className= {styles.buttons}>
           
-          <button id='signin' className="btn btn-warning" onClick={(e)=>{Register(e)}}>Sign In</button>
-          <p type= 'submit' className='signin' >
+          <button type='submit' className="btn btn-warning">Sign In</button>
+          <p  >
           <Link  to="/">
             Already a member? please Log in
           </Link>
           </p>
 
      </div>
-
-          <p id='answer' className='form-group' style={{textAlign:'center'}}>{answer}</p>
+         
 
       
     </form>
