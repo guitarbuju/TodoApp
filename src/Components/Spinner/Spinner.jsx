6a0f92fcb278a16130e86d964ae4e91de5bcb27a
guitarbuju <gerardoamadrid@gmail.com>
2023-06-14 15:05:41 +0200

    spinner image

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -1,17 +1,16 @@
-import  { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import  styles from './spinner.module.css'
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./spinner.module.css";
 
 const Spinner = () => {
   const navigate = useNavigate();
-
+  
   useEffect(() => {
-    const delay = 3000; // Delay in milliseconds
+    const delay = 5000; // Delay in milliseconds
 
     const timeoutId = setTimeout(() => {
       // Navigate to another component after the specified delay
-      navigate('/');
+      navigate("/");
     }, delay);
 
     // Clean up the timeout when the component unmounts
@@ -21,12 +20,13 @@ const Spinner = () => {
   }, [navigate]);
 
   return (
-    <div className={styles.wrapper}>
-
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.ver}>Thank You for visiting....</h1>
         <h1 className={styles.loader}></h1>
-        <h2 className={styles.ver}>Thank You for visiting....</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Spinner;
