1aad2e8e88d1a5a1e3f5c95c3a5a559188387452
guitarbuju <gerardoamadrid@gmail.com>
2023-06-19 19:54:22 +0200

    categories better

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom";
-import Image from "../../assets/gilb.jpeg";
+import Image from "../../assets/categories.003.png";
 import Styles from "./categories.module.css";
 
 const Categories = () => {
