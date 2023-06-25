965a176a3722e39d3be875157f3f45a50eff772b
guitarbuju <gerardoamadrid@gmail.com>
2023-06-26 00:31:21 +0200

    footer completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,25 +1,26 @@
+import styles from "./footer.module.css";
+import social from "../../assets/Untitled/‎Untitled.‎001.png";
+import nuclio from "../../assets/nuclio/‎nuclio.‎001.png";
+import guitar from "../../assets/guitar/‎guitar.png";
+import city from "../../assets/bcn/‎bcn.‎001.png";
+import cityLogo from "../../assets/bcn/‎bcn.‎002.png";
 
-import styles from './footer.module.css'
-import social from '../../assets/Untitled/‎Untitled.‎001.png'
-import nuclio from '../../assets/nuclio/‎nuclio.‎001.png'
-import guitar from '../../assets/guitar/‎guitar.png'
 const Footer = () => {
-
-    
   return (
     <div className={styles.wrapper}>
-   <img className={styles.social} src = {social}/>
-   <div className={styles.G}>
-    
-     <img className={styles.guitar} src = {guitar}/> 
-     <p className={styles.title}>Powered by Gerardo Madrid...</p>
-   </div>
-   
-   <a href='https://nuclio.school/'><img  className={styles.nuclio} src  = {nuclio}/></a> 
+     <img className={styles.social} src={city} />
+      <img className={styles.social} src={social} />
+      <div className={styles.G}>
+        <img className={styles.guitar} src={guitar} />
+        <p className={styles.title}>Powered by Gerardo Madrid...</p>
+      </div>
 
-    
+      <a href="https://nuclio.school/">
+        <img className={styles.nuclio} src={nuclio} />
+      </a>
+      <img className={styles.cityLogo} src={cityLogo} />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
